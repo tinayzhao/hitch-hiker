c5679f25f47b2189912dedd04d4b473368fc8cc5
DanFein <danfeinstat@gmail.com>
2019-06-08 16:59:38 -0700

    added a map component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import Form from "./components/signUpForm/SignUp";
+import Map from "./components/map/Map";
 import "./App.css";
 
 function App() {
   const [formActive, updateFormActive] = useState(false);
   return (
     <div className="App">
-      {/* map goes here */}
+      {!formActive && <Map />}
       {formActive && <Form />}
       {!formActive && (
         <button
---
+++ client/src/components/map/Map.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from "react";
+
+const Map = ({ options, onMount, className }) => {
+  const props = { ref: useRef(), className };
+  const onLoad = () => {
+    const map = new window.google.maps.Map(props.ref.current, options);
+    onMount && onMount(map);
+  };
+
+  useEffect(() => {
+    if (!window.google) {
+      const script = document.createElement(`script`);
+      script.type = `text/javascript`;
+      script.src = `https://maps.google.com/maps/api/js?key=AIzaSyBPTtJb7tqH2YeU4PxLTnRG94GTDmWvRfA`;
+      const headScript = document.getElementsByTagName(`script`)[0];
+      headScript.parentNode.insertBefore(script, headScript);
+      script.addEventListener(`load`, onLoad);
+      return () => script.removeEventListener(`load`, onLoad);
+    } else onLoad();
+  });
+
+  return (
+    <div
+      {...props}
+      style={{ height: `70vh`, margin: `1em 0`, borderRadius: `0.5em` }}
+    />
+  );
+};
+
+Map.defaultProps = {
+  options: {
+    center: { lat: 37.7876, lng: -122.3966 },
+    zoom: 5,
+  },
+};
+
+export default Map;
